9556430985182e555ab068b09fa0421f00aa80da
byteRaj-commits <rajp182138@gmail.com>
2026-01-29 06:50:15 +0530

    make the music play in functionality

--- script.js
+++ script.js
@@ -3,8 +3,29 @@ document.addEventListener("DOMContentLoaded", function(){
     const progress = document.getElementById("progress");
     const song = document.getElementById("song");
     const playIcon = document.getElementById("playIcon");
+    const icon = document.querySelector(".icon");
 
+    song.onloadedmetadata = function(){
+        progress.max = song.duration;
+        progress.value = song.currentTime;
+    }
 
+    
+
+    icon.addEventListener("click", playPause);
+
+    function playPause(){
+        if(playIcon.classList.contains("fa-pause")){
+            song.pause();
+            playIcon.classList.remove("fa-pause");
+            playIcon.classList.add("fa-play");
+        }
+        else{
+            song.play();
+            playIcon.classList.add("fa-pause");
+            playIcon.classList.remove("fa-play");
+        }
+    }
     
    
 })
